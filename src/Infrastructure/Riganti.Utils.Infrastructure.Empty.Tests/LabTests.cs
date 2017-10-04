a27b76aad96d2a2910288f91e76cfeb946f9af5a
Lubos Hladik <github@hlsoft.cz>
2017-10-04 10:17:29 +0200

    Added unit test.

---
+++ src/Infrastructure/Riganti.Utils.Infrastructure.Empty.Tests/LabTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Riganti.Utils.Infrastructure.Empty.Tests
+{
+    public class LabTests
+    {
+        [Fact]
+        public void Test_Succeeds()
+        {
+            Assert.True(true);
+        }
+    }
+}
