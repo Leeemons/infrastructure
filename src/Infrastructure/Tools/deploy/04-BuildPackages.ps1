6004886ef8247efdaa99b26f8ccaf076cb29b7d7
Lubos Hladik <github@hlsoft.cz>
2017-10-12 16:00:02 +0200

    Added missing packages.

--- src/Infrastructure/Tools/deploy/04-BuildPackages.ps1
+++ src/Infrastructure/Tools/deploy/04-BuildPackages.ps1
@@ -17,4 +17,8 @@ function BuildPackages() {
 	}
 }
 
+$packages = @(
+	[pscustomobject]@{ Package = "Riganti.Utils.Infrastructure.Empty"; Directory = "Riganti.Utils.Infrastructure.Empty" },
+)
+
 BuildPackages;
