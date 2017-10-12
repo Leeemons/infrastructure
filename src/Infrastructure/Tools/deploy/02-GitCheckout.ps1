015277bca2518df3fc57584df25f96f733de1c2f
Lubos Hladik <github@hlsoft.cz>
2017-10-12 16:07:51 +0200

    Git Checkout.

--- src/Infrastructure/Tools/deploy/02-GitCheckout.ps1
+++ src/Infrastructure/Tools/deploy/02-GitCheckout.ps1
@@ -1,40 +1,8 @@
 param([String]$branchName, [String]$repoUrl)
 
-### Helper Functions
-
-function Invoke-Git {
-<#
-.Synopsis
-Wrapper function that deals with Powershell's peculiar error output when Git uses the error stream.
-
-.Example
-Invoke-Git ThrowError
-$LASTEXITCODE
-
-#>
-    [CmdletBinding()]
-    param(
-        [parameter(ValueFromRemainingArguments=$true)]
-        [string[]]$Arguments
-    )
-
-    & {
-        [CmdletBinding()]
-        param(
-            [parameter(ValueFromRemainingArguments=$true)]
-            [string[]]$InnerArgs
-        )
-        git.exe $InnerArgs 2>&1
-    } -ErrorAction SilentlyContinue -ErrorVariable fail @Arguments
-
-    if ($fail) {
-        $fail.Exception
-    }
-}
-
 function GitCheckout() {
-	invoke-git checkout $branchName
-	invoke-git -c http.sslVerify=false pull $repoUrl $branchName
+	git checkout $branchName
+	git -c http.sslVerify=false pull $repoUrl $branchName
 }
 
 GitCheckout;
