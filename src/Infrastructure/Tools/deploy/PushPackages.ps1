603c0024ed7e6c32b55ff44a064c33e2274bbd7c
Lubos Hladik <github@hlsoft.cz>
2017-10-12 19:39:11 +0200

    Fix.

--- src/Infrastructure/Tools/deploy/PushPackages.ps1
+++ src/Infrastructure/Tools/deploy/PushPackages.ps1
@@ -1,8 +1,7 @@
 param([String]$apiKey, [String]$server, [string]$enabled)
 
 if (-Not $enabled.ToLower() -eq "true") {
-    Write-Host "Publishing to $server is disabled."
-    return
+  Write-Host "Publishing to $server is disabled."
+} else {
+  dotnet nuget push ".\.nupkgs\*.nupkg" --source $server --api-key $apiKey | Out-Host
 }
-
-dotnet nuget push ".\.nupkgs\*.nupkg" --source $server --api-key $apiKey | Out-Host 
