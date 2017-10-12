cc2aadafb5dafbe2977a32ba92889d88c35edd14
Lubos Hladik <github@hlsoft.cz>
2017-10-12 16:42:55 +0200

    Scripts renamed.

--- src/Infrastructure/Tools/deploy/PackPackages.ps1
+++ src/Infrastructure/Tools/deploy/PackPackages.ps1
@@ -8,13 +8,6 @@ $packages = @(
 foreach ($package in $packages) {
 	cd .\$($package.Directory)
 		
-	if ($nugetRestoreAltSource -eq "") {
-		& dotnet restore | Out-Host
-	}
-	else {
-		& dotnet restore --source $nugetRestoreAltSource --source https://nuget.org/api/v2/ | Out-Host
-	}
-		
 	& dotnet pack --configuration Release --output "..\.nupkgs" | Out-Host
 	cd ..
 }
